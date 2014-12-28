6cd54a996207f2b739dd31596b548f98b04b68f5
hlukovac <hhaarree@hotmail.com>
2014-12-28 15:31:17 +0100

    Added a folder containing my first script

---
+++ Script1/script1.py
@@ -0,0 +1,9 @@
+# My first Python script
+
+import sys				# Load a ibrary module
+print(sys.platform)		# Print the name of the platform in use
+
+print(2**100)			# Print 2 to a power of 100
+
+x = "spam"				# Assing the string "spam" to the variable x
+print(x*8)				# Print the x variable 8 times
